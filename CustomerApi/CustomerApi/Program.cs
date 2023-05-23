517e5f9a56a65116e06fe4613fc702e0e685c079
Shaiju Rajan <shaiju.r@falainacloud.com>
2023-05-23 13:10:32 +0530

    enabled swagger unconditionally

--- CustomerApi/CustomerApi/Program.cs
+++ CustomerApi/CustomerApi/Program.cs
@@ -41,11 +41,11 @@ builder.Services.AddSingleton<ICustomerUpdateSender,CustomerUpdateSender>();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
+//if (app.Environment.IsDevelopment())
+//{
     app.UseSwagger();
     app.UseSwaggerUI();
-}
+//}
 
 //app.UseHttpsRedirection();
 
